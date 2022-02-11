b1a23e441cd2bfaccd388152f11cd6c5de72bb28
Ayush Srihari <ayushsrihari@Ayush-Srihari-B581s-MacBook-Pro.local>
2022-02-11 11:54:56 +0530

    conflicts removed

--- FigmaToCode/code.js
+++ FigmaToCode/code.js
@@ -1,33 +1,14 @@
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
 figma.on("run", () => {
-    const parent = figma.currentPage.children;
-    Array.prototype.forEach.call(parent, child => {
-        if (child.absoluteRenderBounds) {
-            figma.ui.postMessage({
-                type: "CREATE_ELEMENTS",
-                name: child.name,
-                height: child.absoluteRenderBounds.height,
-                width: child.absoluteRenderBounds.width,
-                x: child.absoluteRenderBounds.x,
-                y: child.absoluteRenderBounds.y,
-                // color: {
-                //   r: child.fills.color.r,
-                //   g: child.fills.color.g,
-                //   b: child.fills.color.b,
-                // }
-            });
-        }
-    });
     let divCssProperties = {
         pos_x: null,
         pos_y: null,
-        position: " absolute",
+        position: "absolute",
         width: null,
         height: null,
     };
     const childrenArray = figma.currentPage.children.map((childElement) => {
-        console.log(childElement.x);
         divCssProperties = {
             pos_x: childElement.x,
             pos_y: childElement.y,
@@ -37,7 +18,8 @@ figma.on("run", () => {
         };
         return divCssProperties;
     });
-    console.log(childrenArray);
+    // console.log("childrenArray");
+    // console.log(childrenArray);
     figma.ui.postMessage({
         type: "CREATE_ELEMENTS",
         components: childrenArray,
