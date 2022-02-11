f1c79119594454d40b4f09d6ce5a383c64fbf18f
Ayush Srihari <ayushsrihari@Ayush-Srihari-B581s-MacBook-Pro.local>
2022-02-11 11:20:34 +0530

    style ccaptured

--- FigmaToCode/code.js
+++ FigmaToCode/code.js
@@ -1,5 +1,8 @@
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
+console.log(figma.currentPage.children);
+console.log("color");
+console.log(figma.currentPage.children[0].fills);
 figma.on("run", () => {
     const parent = figma.currentPage.children;
     Array.prototype.forEach.call(parent, child => {
--- FigmaToCode/code.ts
+++ FigmaToCode/code.ts
@@ -1,18 +1,22 @@
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
-
-figma.on("run", () => {  
+figma.on("run", () => {
   const parent = figma.currentPage.children;
   Array.prototype.forEach.call(parent, child => {
     if (child.absoluteRenderBounds) {
       figma.ui.postMessage({
         type: "CREATE_ELEMENTS",
+        name: child.name,
         height: child.absoluteRenderBounds.height,
         width: child.absoluteRenderBounds.width,
         x: child.absoluteRenderBounds.x,
         y: child.absoluteRenderBounds.y,
+        color: {
+          r: child.fills.color.r,
+          g: child.fills.color.g,
+          b: child.fills.color.b,
+        }
       });
     }
   });
-
 });
