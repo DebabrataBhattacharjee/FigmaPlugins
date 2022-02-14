b013c4ae4a8ee4d8ca4811295e736f479823fe35
Ayush Srihari <ayushsrihari@Ayush-Srihari-B581s-MacBook-Pro.local>
2022-02-14 11:32:43 +0530

    condition with color codes

--- FigmaToCode/src/ui.js
+++ FigmaToCode/src/ui.js
@@ -1,54 +1,8 @@
+import { createDiv } from "./utils/createDivElement";
 onmessage = ({ data }) => {
     const { pluginMessage: { components }, } = data;
     console.log("****", components);
     let htmlCode = createDiv(components);
     htmlCode = "<!DOCTYPE html>\n<html>\n<body>" + htmlCode + "</body>\n</html>";
     document.getElementById("div-container").innerText = htmlCode;
-    createDiv(components);
 };
-function createDiv(components) {
-    if (components && components.length) {
-        let htmlCode = "";
-        components.forEach((element) => {
-            htmlCode = htmlCode + getHtmlCodeDiv(element);
-        });
-        return htmlCode;
-    }
-    return "";
-}
-function getHtmlCodeDiv(element) {
-    if (element) {
-        const divHtml = `
-  <div
-      style='
-      background-color: ${getBackgroundColor(element)};
-      width:${element.width};
-      height:${element.height};
-      position:${element.position};
-      top:${element.pos_y};
-      left:${element.pos_x}
-      '>
-            ${createDiv(element.children)}
-  </div>
-  `;
-        return divHtml;
-    }
-    return null;
-}
-function getBackgroundColor(element) {
-    if (element.color) {
-        return `rgb(${element.color.r},${element.color.g},${element.color.b})`;
-    }
-    return "none";
-}
-// document.getElementById("create").onclick = () => {
-//   const textbox = document.getElementById("count") as HTMLInputElement;
-//   const count = parseInt(textbox.value, 10);
-//   parent.postMessage(
-//     { pluginMessage: { type: "create-rectangles", count } },
-//     "*"
-//   );
-// };
-// document.getElementById("cancel").onclick = () => {
-//   parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
-// };
---
+++ FigmaToCode/src/utils/createDivElement.js
@@ -0,0 +1,11 @@
+import { getHtmlCodeDiv } from "./getHtmlCodeDiv";
+export const createDiv = (components) => {
+    if (components && components.length) {
+        let htmlCode = "";
+        components.forEach((element) => {
+            htmlCode = htmlCode + getHtmlCodeDiv(element);
+        });
+        return htmlCode;
+    }
+    return "";
+};
